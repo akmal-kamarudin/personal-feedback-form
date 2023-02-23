e75c88fe9ee2fbf143c253207050c9450c5b17ef
Akmal Kamarudin <akmal.kamarudin19@gmail.com>
2023-02-23 20:17:52 +0800

    feedback form done

---
+++ about.php
@@ -0,0 +1,8 @@
+<?php include 'inc/header.php'; ?>
+    <h2>About</h2>
+
+    <p class="text-center">It's a mini project that I made while following from a tutorial on YouTube. <br> 
+    Do leave a feedback or just drop by a comment to say hello :)</p>
+
+<?php include 'inc/footer.php'; ?>
+
